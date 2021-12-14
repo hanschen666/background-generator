63306939eb7c6faa5c0fa2229478e1c20b79c84b
hanschen666 <15814841133@163.com>
2021-12-14 22:56:46 +0800

    adding starting project

---
+++ script.js
@@ -0,0 +1,14 @@
+let color1 = document.querySelector("#color1");
+let color2 = document.querySelector("#color2");
+let body = document.querySelector("#gradient");
+let css = document.querySelector("h4");
+
+function setGradientColor() {
+  body.style.background =
+    "linear-gradient(to right," + color1.value + "," + color2.value + ")";
+
+  css.textContent = body.style.background;
+}
+
+color1.addEventListener("input", setGradientColor);
+color2.addEventListener("input", setGradientColor);
